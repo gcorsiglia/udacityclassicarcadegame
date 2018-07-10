5c68bbdb5ba8594ad5b83614c2524a00f6f2f782
Gina Corsiglia <gina@ginasaysyes.com>
2018-07-09 18:19:11 -0700

    Pseudo code and notes

--- js/app.js
+++ js/app.js
@@ -1,41 +1,101 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/*
+ * ENEMY CLASS
+ */
+class Enemy {
+    constructor() {
+        // Set image
+        this.sprite = 'images/enemy-bug.png';
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        // Rest of properties and variables go here
+            // Set initial location
+                // x pos
+                // y pos
+            // Set speed
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // Methods go here
+    
+    // Update enemy's position
+    update(dt) {
+        // Parameter: dt, a time delta between ticks
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+        // Multiply any movement by the dt parameter
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+        // If enemy is not paseed boundary
+            // Move forward
+            // Increment x by speed * dt
+        // else
+            // Resest position to start
+    }
+
+    // Draw the enemy on screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+/*
+ * PLAYER CLASS
+ */
+class Player {
+    constructor() {
+        // Set image
+        this.sprite = 'images/char-boy.png';
+
+        // Rest of properties and variables go here
+            // Set location
+                // x pos
+                // y pos
+    }
+
+    // Methods go here
+
+    // Update Player's position
+    update(dt) {
+        // Multiply any movement by the dt parameter
+
+        // Check for collison
+            // Did Player's x and y collide with enemy?
+
+        // Check for win
+            // Did player's x and y reach final tile?
+                // IF yes, reset game
+    }
+
+    // Draw the player on screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    // Move Player based on user input
+    handleInput() {
+        // Receive key presses + allowedKeys
+        // Update Player's x and y according to input
+        // NO moving offscreen
+    }
+
+    // Reset Player
+    reset() {
+        // Set x and y to starting x an y
+    }
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/* 
+ * INSTANTIATE OBJECTS
+ */
+const allEnemies = [];
+// Create multiple new enemy objects
+    // Use loop to create up to certain number
+// Push each enemy object to allEnemies array
 
-
+// Player object
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
